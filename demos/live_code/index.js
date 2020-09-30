f180b92af25bd561d4c23b0514e1c767a023dfe9
dracco1993 <dracco1993@gmail.com>
2020-09-30 16:05:24 -0400

    Add other languages, and a language dropdown

--- demos/live_code/index.js
+++ demos/live_code/index.js
@@ -32,15 +32,19 @@ Blockly.svgResize(workspace);
 
 // Update code on block change
 function onWorkspaceChange(event) {
-  // var languageDropdown = document.getElementById('languageDropdown');
-  // var languageSelection = languageDropdown.options[languageDropdown.selectedIndex].value;
+  var languageDropdown = document.getElementById('languageDropdown');
+  var languageSelection = languageDropdown.options[languageDropdown.selectedIndex].value;
+
   var codeDiv = document.getElementById('codeArea');
+
   var codeHolder = document.createElement('pre');
   codeHolder.className = 'prettyprint but-not-that-pretty';
-  // var code = document.createTextNode(Blockly[languageSelection].workspaceToCode(workspace));
-  var code = document.createTextNode(Blockly["JavaScript"].workspaceToCode(workspace));
+
+  var code = document.createTextNode(Blockly[languageSelection].workspaceToCode(workspace));
+
   codeHolder.appendChild(code);
   codeDiv.replaceChild(codeHolder, codeDiv.lastElementChild);
+
   prettyPrint();
 }
 workspace.addChangeListener(onWorkspaceChange);
