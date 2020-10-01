fed8afa34341a309c29f3f630305016dd78d7966
dracco1993 <dracco1993@gmail.com>
2020-10-01 15:50:16 -0400

    Add java types file

---
+++ typings/java.d.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright 2020 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Type definitions for the Java generator.
+ * @author dracco1993@gmail.com (Jordan Miller)
+ */
+
+/// <reference path="core.d.ts" />
+
+import * as Blockly from './core';
+export = Blockly.Generator;
